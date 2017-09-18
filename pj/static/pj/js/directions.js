6f03761b2a7e418facfffa8d181cc8dfa53fb49d
Bob-Calco <bob.luursema@outlook.com>
2017-09-18 23:28:09 +0200

    Hotfix

--- pj/static/pj/js/directions.js
+++ pj/static/pj/js/directions.js
@@ -1,5 +1,5 @@
 function initMap() {
-        var uluru = {lat: 51.9952431, lng: 5.8897835};
+        var uluru = {lat: 51.9952667, lng: 5.8917199};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 13,
           center: uluru
