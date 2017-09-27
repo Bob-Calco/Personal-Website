d66ea8e71b4aaceb4b3b9a8c47c9f0fd4008af47
Bob-Calco <bob.luursema@outlook.com>
2017-09-27 22:31:45 +0200

    Major redesign of pjubeljaar

--- pj/static/pj/js/base.js
+++ pj/static/pj/js/base.js
@@ -1,9 +1,12 @@
 function toggleMenu(){
-  var nav = document.getElementById('nav-links')
+  var nav = document.getElementById('nav-links');
+  var content = document.getElementById('content');
   if (nav.classList.contains('nav-active')){
     nav.classList.remove('nav-active');
+    content.classList.remove('hidden');
   }
   else {
     nav.classList.add('nav-active');
+    content.classList.add('hidden');
   }
 }
